f1b56cbc46cb6075b66e00c306b2d6d1b6ff5499
Edmund Tsou <etsou2@ugrad18.cs.jhu.edu>
2026-04-17 18:01:57 -0400

    added wire::encode and wire::decode functions and related helpers

--- src/wire.cpp
+++ src/wire.cpp
@@ -78,10 +78,98 @@ const std::unordered_map<std::string, OrderStatus> s_str_to_order_status(
   s_str_to_order_status_vec.end()
 );
 
-// TODO: add private helper functions.
-// Suggestion: functions to help encode and decode components
-// of messages (in support of the Wire::encode and Wire::decode
-// functions) would be a good idea.
+// encode a single item into the colon separated payload form used by orders
+std::string encode_item(const Item &item) {
+  return std::to_string(item.get_order_id()) + ":" + std::to_string(item.get_id()) + ":" + s_item_status_to_str.at(item.get_status()) + ":" + item.get_desc() + ":" + std::to_string(item.get_qty());
+}
+
+// encode an order and all of its items into the comma/semicolon separated payload form
+std::string encode_order(const std::shared_ptr<Order> &order) {
+  assert(order);
+  if (order->get_num_items() == 0)
+    throw InvalidMessage("order must contain at least one item");
+  std::string result = std::to_string(order->get_id()) + "," + s_order_status_to_str.at(order->get_status()) + ",";
+
+  for (int i = 0; i < order->get_num_items(); ++i) {
+    if (i > 0)
+      result += ";";
+    result += encode_item(*order->at(i));
+  }
+
+  return result;
+}
+
+// Parse a base-10 integer and reject partial parses or other malformed values.
+int parse_int(const std::string &s) {
+  if (s.empty())
+    throw InvalidMessage("invalid integer");
+
+  size_t pos = 0;
+  int value = 0;
+
+  try {
+    value = std::stoi(s, &pos, 10);
+  } catch (...) {
+    throw InvalidMessage("invalid integer");
+  }
+
+  if (pos != s.size())
+    throw InvalidMessage("invalid integer");
+
+  return value;
+}
+
+// Parse a strictly positive integer.
+int parse_positive_int(const std::string &s) {
+  int value = parse_int(s);
+  if (value <= 0)
+    throw InvalidMessage("invalid positive integer");
+  return value;
+}
+
+// decode a single item from the colon-separated payload form
+std::shared_ptr<Item> decode_item(const std::string &s) {
+  auto parts = Util::split(s, ':');
+  if (parts.size() != 5)
+    throw InvalidMessage("invalid item encoding");
+
+  int order_id = parse_positive_int(parts[0]);
+  int item_id = parse_positive_int(parts[1]);
+  auto item_status_it = s_str_to_item_status.find(parts[2]);
+  ItemStatus status = ItemStatus::INVALID;
+  if (item_status_it != s_str_to_item_status.end())
+    status = item_status_it->second;
+  if (status == ItemStatus::INVALID)
+    throw InvalidMessage("invalid item status");
+
+  int qty = parse_positive_int(parts[4]);
+  return std::make_shared<Item>(order_id, item_id, status, parts[3], qty);
+}
+
+// decode an order and its items from the comma/semicolon separated payload form
+std::shared_ptr<Order> decode_order(const std::string &s) {
+  auto parts = Util::split(s, ',');
+  if (parts.size() != 3)
+    throw InvalidMessage("invalid order encoding");
+
+  int order_id = parse_positive_int(parts[0]);
+  auto order_status_it = s_str_to_order_status.find(parts[1]);
+  OrderStatus status = OrderStatus::INVALID;
+  if (order_status_it != s_str_to_order_status.end())
+    status = order_status_it->second;
+  if (status == OrderStatus::INVALID)
+    throw InvalidMessage("invalid order status");
+
+  if (parts[2].empty())
+    throw InvalidMessage("order must contain at least one item");
+
+  auto order = std::make_shared<Order>(order_id, status);
+  auto item_strs = Util::split(parts[2], ';');
+  for (const auto &item_str : item_strs)
+    order->add_item(decode_item(item_str));
+
+  return order;
+}
 
 
 } // end of anonymous namespace for helper functions
@@ -90,50 +178,175 @@ namespace Wire {
 
 std::string client_mode_to_str(ClientMode client_mode) {
   auto i = s_client_mode_to_str.find(client_mode);
-  return (i != s_client_mode_to_str.end()) ? i->second : "<invalid>";
+  if (i != s_client_mode_to_str.end())
+    return i->second;
+  return "<invalid>";
 }
 
 ClientMode str_to_client_mode(const std::string &s) {
   auto i = s_str_to_client_mode.find(s);
-  return (i != s_str_to_client_mode.end()) ? i->second : ClientMode::INVALID;
+  if (i != s_str_to_client_mode.end())
+    return i->second;
+  return ClientMode::INVALID;
 }
 
 std::string message_type_to_str(MessageType message_type) {
   auto i = s_message_type_to_str.find(message_type);
-  return (i != s_message_type_to_str.end()) ? i->second : "<invalid>";
+  if (i != s_message_type_to_str.end())
+    return i->second;
+  return "<invalid>";
 }
 
 MessageType str_to_message_type(const std::string &s) {
   auto i = s_str_to_message_type.find(s);
-  return (i != s_str_to_message_type.end()) ? i->second : MessageType::INVALID;
+  if (i != s_str_to_message_type.end())
+    return i->second;
+  return MessageType::INVALID;
 }
 
 std::string item_status_to_str(ItemStatus item_status) {
   auto i = s_item_status_to_str.find(item_status);
-  return (i != s_item_status_to_str.end()) ? i->second : "<invalid>";
+  if (i != s_item_status_to_str.end())
+    return i->second;
+  return "<invalid>";
 }
 
 ItemStatus str_to_item_status(const std::string &s) {
   auto i = s_str_to_item_status.find(s);
-  return (i != s_str_to_item_status.end()) ? i->second : ItemStatus::INVALID;
+  if (i != s_str_to_item_status.end())
+    return i->second;
+  return ItemStatus::INVALID;
 }
 
 std::string order_status_to_str(OrderStatus order_status) {
   auto i = s_order_status_to_str.find(order_status);
-  return (i != s_order_status_to_str.end()) ? i->second : "<invalid>";
+  if (i != s_order_status_to_str.end())
+    return i->second;
+  return "<invalid>";
 }
 
 OrderStatus str_to_order_status(const std::string &s) {
   auto i = s_str_to_order_status.find(s);
-  return (i != s_str_to_order_status.end()) ? i->second : OrderStatus::INVALID;
+  if (i != s_str_to_order_status.end())
+    return i->second;
+  return OrderStatus::INVALID;
 }
 
 void encode(const Message &msg, std::string &s) {
-  // TODO: implement
+  s = message_type_to_str(msg.get_type());
+
+  switch (msg.get_type()) {
+    case MessageType::LOGIN:
+      s += "|" + client_mode_to_str(msg.get_client_mode()) + "|" + msg.get_str();
+      break;
+
+    case MessageType::QUIT:
+    case MessageType::OK:
+    case MessageType::ERROR:
+      s += "|" + msg.get_str();
+      break;
+
+    case MessageType::ORDER_NEW:
+    case MessageType::DISP_ORDER:
+      s += "|" + encode_order(msg.get_order());
+      break;
+
+    case MessageType::ITEM_UPDATE:
+    case MessageType::DISP_ITEM_UPDATE:
+      s += "|" + std::to_string(msg.get_order_id()) + "|" +
+           std::to_string(msg.get_item_id()) + "|" +
+           item_status_to_str(msg.get_item_status());
+      break;
+
+    case MessageType::ORDER_UPDATE:
+    case MessageType::DISP_ORDER_UPDATE:
+      s += "|" + std::to_string(msg.get_order_id()) + "|" +
+           order_status_to_str(msg.get_order_status());
+      break;
+
+    case MessageType::DISP_HEARTBEAT:
+    case MessageType::INVALID:
+      break;
+  }
 }
 
 void decode(const std::string &s, Message &msg) {
-  // TODO: implement
+  auto parts = Util::split(s, '|');
+  if (parts.empty())
+    throw InvalidMessage("empty message");
+
+  MessageType type = str_to_message_type(parts[0]);
+  if (type == MessageType::INVALID)
+    throw InvalidMessage("invalid message type");
+
+  Message result;
+  result.set_type(type);
+
+  switch (type) {
+    case MessageType::LOGIN:
+      if (parts.size() != 3)
+        throw InvalidMessage("invalid login message");
+      {
+        ClientMode client_mode = Wire::str_to_client_mode(parts[1]);
+        if (client_mode == ClientMode::INVALID)
+          throw InvalidMessage("invalid client mode");
+        result.set_client_mode(client_mode);
+        result.set_str(parts[2]);
+      }
+      break;
+
+    case MessageType::QUIT:
+    case MessageType::OK:
+    case MessageType::ERROR:
+      if (parts.size() != 2)
+        throw InvalidMessage("invalid string message");
+      result.set_str(parts[1]);
+      break;
+
+    case MessageType::ORDER_NEW:
+    case MessageType::DISP_ORDER:
+      if (parts.size() != 2)
+        throw InvalidMessage("invalid order message");
+      result.set_order(decode_order(parts[1]));
+      break;
+
+    case MessageType::ITEM_UPDATE:
+    case MessageType::DISP_ITEM_UPDATE:
+      if (parts.size() != 4)
+        throw InvalidMessage("invalid item update message");
+      result.set_order_id(parse_positive_int(parts[1]));
+      result.set_item_id(parse_positive_int(parts[2]));
+      {
+        ItemStatus item_status = Wire::str_to_item_status(parts[3]);
+        if (item_status == ItemStatus::INVALID)
+          throw InvalidMessage("invalid item status");
+        result.set_item_status(item_status);
+      }
+      break;
+
+    case MessageType::ORDER_UPDATE:
+    case MessageType::DISP_ORDER_UPDATE:
+      if (parts.size() != 3)
+        throw InvalidMessage("invalid order update message");
+      result.set_order_id(parse_positive_int(parts[1]));
+      {
+        OrderStatus order_status = Wire::str_to_order_status(parts[2]);
+        if (order_status == OrderStatus::INVALID)
+          throw InvalidMessage("invalid order status");
+        result.set_order_status(order_status);
+      }
+      break;
+
+    case MessageType::DISP_HEARTBEAT:
+      if (parts.size() != 1)
+        throw InvalidMessage("invalid heartbeat message");
+      break;
+
+    case MessageType::INVALID:
+      throw InvalidMessage("invalid message type");
+  }
+
+  msg = result;
 }
 
 }
